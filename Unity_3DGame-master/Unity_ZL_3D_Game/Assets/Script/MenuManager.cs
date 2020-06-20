742170008423be7576308e06e0f552f4568f54f6
CMTen <57666274+CMTen@users.noreply.github.com>
2020-06-20 14:53:51 +0800

    update-UI

--- Unity_3DGame-master/Unity_ZL_3D_Game/Assets/Script/MenuManager.cs
+++ Unity_3DGame-master/Unity_ZL_3D_Game/Assets/Script/MenuManager.cs
@@ -6,6 +6,13 @@ public class MenuManager : MonoBehaviour
     [Header("玩家資料")]
     public PlayerData dataPlayer;
 
+    public GameObject sword;
+    public GameObject swordOn;
+    public GameObject swordUI;
+    public GameObject armor;
+    public GameObject armorOn;
+    public GameObject armorUI;
+
     public void BuyHp_100()
     {
         dataPlayer.maxHp += 100;
@@ -21,5 +28,49 @@ public class MenuManager : MonoBehaviour
     {
         dataPlayer.hp = dataPlayer.maxHp;
         SceneManager.LoadScene("關卡1");
+    }
+
+    public void showSword()
+    {
+        sword.SetActive(true);
+    }
+
+    public void closeSword()
+    {
+        sword.SetActive(false);
+    }
+
+    public void wearSword()
+    {
+        swordOn.SetActive(true);
+        swordUI.SetActive(true);
+    }
+
+    public void takeoffSword()
+    {
+        swordOn.SetActive(false);
+        swordUI.SetActive(false);
+    }
+
+    public void showArmor()
+    {
+        armor.SetActive(true);
+    }
+
+    public void closeArmor()
+    {
+        armor.SetActive(false);
+    }
+
+    public void wearArmor()
+    {
+        armorOn.SetActive(true);
+        armorUI.SetActive(true);
+    }
+
+    public void takeofArmor()
+    {
+        armorOn.SetActive(false);
+        armorUI.SetActive(false);
     }
 }
